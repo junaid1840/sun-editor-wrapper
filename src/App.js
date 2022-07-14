896d2055da56947038789b2761b5a223b16da71a
junaidnz <junaid.nazir@tintash.com>
2022-07-14 17:20:27 +0500

    PMPB-37 - Initialized react app and added a SunEditor empty component.

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import {SunEditor} from "./components/sun-editor/SunEditor";
+
+function App() {
+  return (
+      <SunEditor />
+  );
+}
+
+export default App;
---
+++ src/components/sun-editor/SunEditor.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const SunEditor = () => {
+    return (
+        <h1>Sun Editor</h1>
+    )
+}
